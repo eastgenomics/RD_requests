6ab163a31c3dcc1942bdcf69498cf8e002d4ce06
RSWilson1 <robert.s.wilson1996@gmail.com>
2024-11-18 12:10:34 +0000

    Added quoted variable to conform to shellcheck styleguide.

--- DI-435/merge_VCF_AF.sh
+++ DI-435/merge_VCF_AF.sh
@@ -15,28 +15,28 @@ to_download=($project_file)
 # Download all vcfs
 for i in "${to_download[@]}"
     do
-        echo $i
-        dx download $i
+        echo "$i"
+        dx download "$i"
     done
 
 # Index VCFs
 echo "Indexing VCFs"
 for vcf in $(ls *vcf.gz); do
-    bcftools index $vcf;
+    bcftools index "$vcf";
 done
 
 # Normalising VCFs
 mkdir norm
 echo "Normalising VCFs"
 for vcf in $(ls *vcf.gz); do
-    bcftools norm -m -any -f ${genome} -Oz $vcf > norm/$vcf;
+    bcftools norm -m -any -f "${genome}" -Oz "$vcf" > norm/"$vcf";
 done
 
 # Indexing normalised VCFs
 echo "Indexing normalised VCFs"
 cd norm
 for vcf in $(ls *vcf.gz); do
-    bcftools index -f $vcf;
+    bcftools index -f "$vcf";
 done
 
 # Merging normalised VCFs
@@ -48,7 +48,7 @@ for vcf in $(ls *vcf.gz); do
 done
 command="${command} > ../merged.vcf"
 echo "${command}"
-eval $command
+eval "${command}"
 
 # Bgzip and index merged VCF file
 echo "Bgzip and indexing merged file"
@@ -62,7 +62,7 @@ command="bcftools norm -m -any -f ${genome} -Ou merged.vcf.gz"
 command="${command} | bcftools +fill-tags --output-type v -o merge_tag.vcf -- -t AN,AC,NS,AF,MAF,AC_Hom,AC_Het,AC_Hemi"
 command="${command} ; bcftools sort merge_tag.vcf -Oz > final_merged_${job}.vcf.gz"
 command="${command} ; tabix -p vcf final_merged_${job}.vcf.gz"
-eval $command
-dx upload final_merged_${job}.vcf.gz
-dx upload final_merged_${job}.vcf.gz.tbi
-dx terminate ${job}
+eval "$command"
+dx upload "final_merged_${job}.vcf.gz"
+dx upload "final_merged_${job}.vcf.gz.tbi"
+dx terminate "${job}"
