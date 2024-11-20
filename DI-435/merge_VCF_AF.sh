0a530e6d36fffa8a1997d0751635803a2de32b30
RSWilson1 <robert.s.wilson1996@gmail.com>
2024-11-20 16:54:17 +0000

    Added new code to create an array of files to download. Added checking for empty input file.

--- DI-435/merge_VCF_AF.sh
+++ DI-435/merge_VCF_AF.sh
@@ -9,33 +9,45 @@
 input_file=$1
 job=$2
 genome=$3
-project_file=$(awk -F ' ' '{print $3":"$4}' "$input_file")
-to_download=("$project_file")
 
-# Download all vcfs
-for i in "${to_download[@]}"
-    do
-        echo "$i"
-        dx download "$i"
-    done
+# Check if the input file exists and is not empty
+if [ ! -s "$input_file" ]; then
+    echo "Input file is empty or does not exist."
+    exit 1
+fi
+
+#blank array to store the project files
+project_files=()
+
+# Read the input file line by line and construct the project_file array
+
+while IFS=$'\t' read -r _ _ field3 field4 _; do
+        project_files+=("${field3}:${field4}")
+done < "$input_file"
+
+echo "Downloading files"
+for i in "${project_files[@]}"; do
+    echo "Processing: $i"
+    dx download $i
+done
 
 # Index VCFs
 echo "Indexing VCFs"
-for vcf in ./*vcf.gz; do
+for vcf in *vcf.gz; do
     bcftools index "$vcf";
 done
 
 # Normalising VCFs
 mkdir norm
 echo "Normalising VCFs"
-for vcf in ./*vcf.gz; do
+for vcf in *vcf.gz; do
     bcftools norm -m -any -f "${genome}" -Oz "$vcf" > norm/"$vcf";
 done
 
 # Indexing normalised VCFs
 echo "Indexing normalised VCFs"
 cd norm || exit
-for vcf in ./*vcf.gz; do
+for vcf in *vcf.gz; do
     bcftools index -f "$vcf";
 done
 
@@ -43,12 +55,12 @@ done
 echo "Merging normalised VCFs"
 command="bcftools merge --output-type v -m none --missing-to-ref"
 # Add the VCF files names to the command
-for vcf in ./*vcf.gz; do
+for vcf in *vcf.gz; do
     command="${command} $vcf";
 done
 command="${command} > ../merged.vcf"
 echo "${command}"
-eval "${command}"
+eval "$command"
 
 # Bgzip and index merged VCF file
 echo "Bgzip and indexing merged file"
@@ -56,7 +68,7 @@ cd ..
 bgzip merged.vcf
 bcftools index merged.vcf.gz
 
-# Final merging and processing
+Final merging and processing
 echo "Final processing"
 command="bcftools norm -m -any -f ${genome} -Ou merged.vcf.gz"
 command="${command} | bcftools +fill-tags --output-type v -o merge_tag.vcf -- -t AN,AC,NS,AF,MAF,AC_Hom,AC_Het,AC_Hemi"
