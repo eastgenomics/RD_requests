2e8b441ae8caf274820babf2af925fd0f7133dea
AmanahLewis-Wade <amanah.lewis-wade@nhs.net>
2025-11-11 15:01:37 +0000

    inlcude validated inputs and correct typos

--- DI-2556/add_headers.sh
+++ DI-2556/add_headers.sh
@@ -15,11 +15,22 @@ new_static_bed_path="/home/dnanexus/new_static_beds"
 new_version=$3
 mkdir -p "${new_static_bed_path}" 
 
+# validate inputs 
+# --- Check argument count ---
+if [[ $# -ne 3 ]]; then
+  echo "Usage: $0 <old_static_bed_path> <old_version> <new_version>"
+  exit 1
+fi
+
+# --- Validate directory ---
+if [[ ! -d "$old_static_bed_path" ]]; then
+  echo "❌ Error: '$old_static_bed_path' is not a valid directory."
+  exit 1
+fi
+
 #update old static bed file by adding a header detailing the updated versions and save in new static file folder.
 # Then use diff to compare old and new static bed files 
 for old_panel_bed_path in ${old_static_bed_path}* ; do
-  let count++
-  printf  "Number of static beds being processed: %d\n" "$count"
 
   # get filename and update the version to v1.0.1 in the filename and make file path for new static bed 
   old_basename=$(basename "$old_panel_bed_path")
@@ -30,6 +41,7 @@ for old_panel_bed_path in ${old_static_bed_path}* ; do
   # add header and save new static bed file
   header="#assembly=GRCh38,version=${new_version}"
   if sed  "1i ${header}" "${old_panel_bed_path}" >  "${new_panel_bed_path}"; then 
+    let count++
     printf  "New bed panel file path,  %s saved \n"  "$new_panel_bed_path"
     #Save diff result in a file 
     diff_output=$(diff -s "${old_panel_bed_path}" "${new_panel_bed_path}")
@@ -39,3 +51,5 @@ for old_panel_bed_path in ${old_static_bed_path}* ; do
   fi 
 
 done
+
+printf  "Total number of static beds processed: %d\n" "$count"
