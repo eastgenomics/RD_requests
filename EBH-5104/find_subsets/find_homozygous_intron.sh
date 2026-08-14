9be2b0ec7b66dc379fdbe69e886df93a1cfa0231
Emma-MacK <emma.mackenzie10@nhs.net>
2026-08-14 14:14:57 +0100

    add handling for old TWE count annotation

--- EBH-5104/find_subsets/find_homozygous_intron.sh
+++ EBH-5104/find_subsets/find_homozygous_intron.sh
@@ -2,27 +2,48 @@
 
 # get homozygous variants b38
 for sample in $(bcftools query -l lepr_variants_TWE_38_sorted_annotated_filtered.vcf.gz);do
-    bcftools query \
+    old_annotation_vatiants=$(bcftools query \
     -s "$sample" \
     -i 'GT="AA" & CSQ_Consequence="intron_variant"' \
     -f '[%SAMPLE]\t%CHROM\t%POS\t%REF\t%ALT\t[%GT]\t%FILTER\t%CSQ_Consequence\t%CSQ_gnomADg_AF\t%CSQ_gnomADe_AF\t%CSQ_TWE_AF\n' \
-    lepr_variants_TWE_38_sorted_annotated_filtered.vcf.gz ||     bcftools query \
-    -s "$sample" \
-    -i 'GT="AA" & CSQ_Consequence="intron_variant"' \
-    -f '[%SAMPLE]\t%CHROM\t%POS\t%REF\t%ALT\t[%GT]\t%FILTER\t%CSQ_Consequence\t%CSQ_gnomADg_AF\t%CSQ_gnomADe_AF\t%CSQ_TWE_WES_v1_AF\n' \
-    lepr_variants_TWE_38_sorted_annotated_filtered.vcf.gz | uniq >>  homozygous_intron_38.txt 
+    lepr_variants_TWE_38_sorted_annotated_filtered.vcf.gz) 
+    if [ "$old_annotation_vatiants" = "" ]; then
+        new_annotation_vatiants=$(bcftools query \
+        -s "$sample" \
+        -i 'GT="AA" & CSQ_Consequence="intron_variant"' \
+        -f '[%SAMPLE]\t%CHROM\t%POS\t%REF\t%ALT\t[%GT]\t%FILTER\t%CSQ_Consequence\t%CSQ_gnomADg_AF\t%CSQ_gnomADe_AF\t%CSQ_TWE_WES_v1_AF\n' \
+        lepr_variants_TWE_38_sorted_annotated_filtered.vcf.gz | uniq)
+        if [ "$new_annotation_vatiants" != "" ]; then
+        echo "$sample" uses new annotation >> homozygous_intron_38.txt
+        echo "$new_annotation_vatiants" >> homozygous_intron_38.txt
+        fi
+
+    else
+        echo "$sample" uses old annotation >> homozygous_intron_38.txt
+        echo "$old_annotation_vatiants" >> homozygous_intron_38.txt
+    fi
 done
 
 
 # get homozygous variants b37
 for sample in $(bcftools query -l lepr_variants_TWE_37_sorted_annotated_filtered.vcf.gz);do
-    bcftools query \
+    old_annotation_vatiants=$(bcftools query \
     -s "$sample" \
     -i 'GT="AA" & CSQ_Consequence="intron_variant"' \
     -f '[%SAMPLE]\t%CHROM\t%POS\t%REF\t%ALT\t[%GT]\t%FILTER\t%CSQ_Consequence\t%CSQ_gnomADg_AF\t%CSQ_gnomADe_AF\t%CSQ_TWE_AF\n' \
-    lepr_variants_TWE_37_sorted_annotated_filtered.vcf.gz ||     bcftools query \
-    -s "$sample" \
-    -i 'GT="AA" & CSQ_Consequence="intron_variant"' \
-    -f '[%SAMPLE]\t%CHROM\t%POS\t%REF\t%ALT\t[%GT]\t%FILTER\t%CSQ_Consequence\t%CSQ_gnomADg_AF\t%CSQ_gnomADe_AF\t%CSQ_TWE_WES_v1_AF\n' \
-    lepr_variants_TWE_37_sorted_annotated_filtered.vcf.gz | uniq >>  homozygous_intron_37.txt 
+    lepr_variants_TWE_37_sorted_annotated_filtered.vcf.gz | uniq)
+    if [ "$old_annotation_vatiants" = "" ]; then
+        new_annotation_vatiants=$(bcftools query \
+        -s "$sample" \
+        -i 'GT="AA" & CSQ_Consequence="intron_variant"' \
+        -f '[%SAMPLE]\t%CHROM\t%POS\t%REF\t%ALT\t[%GT]\t%FILTER\t%CSQ_Consequence\t%CSQ_gnomADg_AF\t%CSQ_gnomADe_AF\t%CSQ_TWE_WES_v1_AF\n' \
+        lepr_variants_TWE_37_sorted_annotated_filtered.vcf.gz | uniq)
+        if [ "$new_annotation_vatiants" != "" ]; then
+            echo "$sample" uses new annotation >> homozygous_intron_37.txt
+            echo "$new_annotation_vatiants" >> homozygous_intron_37.txt
+        fi
+    else
+        echo "$sample" uses old annotation >> homozygous_intron_37.txt
+        echo "$old_annotation_vatiants" >> homozygous_intron_37.txt
+    fi
 done
--- EBH-5104/find_subsets/find_single_het_intron.sh
+++ EBH-5104/find_subsets/find_single_het_intron.sh
@@ -16,20 +16,32 @@ for sample in $(bcftools query -l lepr_variants_TWE_38_sorted_annotated_filtered
     fi
     if [ $count -eq 1 ]; then
         
-        intron_variants=$(bcftools query \
+        intron_variants_old_annotation=$(bcftools query \
             -s "$sample" \
             -i 'GT!="0/0" &CSQ_Consequence="intron_variant"' \
             -f '[%SAMPLE]\t%CHROM\t%POS\t%REF\t%ALT\t[%GT]\t%FILTER\t%CSQ_Consequence\t%CSQ_gnomADg_AF\t%CSQ_gnomADe_AF\t%CSQ_TWE_AF\n' \
-            lepr_variants_TWE_38_sorted_annotated_filtered.vcf.gz|| bcftools query \
-            -s "$sample" \
-            -i 'GT!="0/0" &CSQ_Consequence="intron_variant"' \
-            -f '[%SAMPLE]\t%CHROM\t%POS\t%REF\t%ALT\t[%GT]\t%FILTER\t%CSQ_Consequence\t%CSQ_gnomADg_AF\t%CSQ_gnomADe_AF\t%CSQ_TWE_WES_v1_AF\n' \
-            lepr_variants_TWE_38_sorted_annotated_filtered.vcf.gz | uniq )
-        count_intron=$(echo "$intron_variants" | wc -l)
-        if [ $count_intron -ge 1 ]; then
-            echo "$hets" >> single_het_intron_38.txt
-            echo "$intron_variants" >> single_het_intron_38.txt
+            lepr_variants_TWE_38_sorted_annotated_filtered.vcf.gz| uniq)
+            if [ "$intron_variants_old_annotation" = "" ]; then
+                intron_variants_new_annotation=$(bcftools query \
+                -s "$sample" \
+                -i 'GT!="0/0" &CSQ_Consequence="intron_variant"' \
+                -f '[%SAMPLE]\t%CHROM\t%POS\t%REF\t%ALT\t[%GT]\t%FILTER\t%CSQ_Consequence\t%CSQ_gnomADg_AF\t%CSQ_gnomADe_AF\t%CSQ_TWE_WES_v1_AF\n' \
+                lepr_variants_TWE_38_sorted_annotated_filtered.vcf.gz | uniq )
+
+                count_intron=$(echo "$intron_variants_new_annotation" | wc -l)
+                if [ $count_intron -ge 1 ]; then
+                    echo "$hets" >> single_het_intron_38.txt
+                    echo "$intron_variants_new_annotation" >> single_het_intron_38.txt
+                fi
+            else
+
+                count_intron=$(echo "$intron_variants_old_annotation" | wc -l)
+                if [ $count_intron -ge 1 ]; then
+                    echo "$hets" >> single_het_intron_38.txt
+                    echo "$intron_variants_old_annotation" >> single_het_intron_38.txt
+                fi
         fi
+
 
     # print intronic variants
 
@@ -53,22 +65,32 @@ for sample in $(bcftools query -l lepr_variants_TWE_37_sorted_annotated_filtered
     fi
     if [ $count -eq 1 ]; then
         
-        intron_variants=$(bcftools query \
+        intron_variants_old_annotation=$(bcftools query \
             -s "$sample" \
             -i 'GT!="0/0" &CSQ_Consequence="intron_variant"' \
             -f '[%SAMPLE]\t%CHROM\t%POS\t%REF\t%ALT\t[%GT]\t%FILTER\t%CSQ_Consequence\t%CSQ_gnomADg_AF\t%CSQ_gnomADe_AF\t%CSQ_TWE_AF\n' \
-            lepr_variants_TWE_37_sorted_annotated_filtered.vcf.gz|| bcftools query \
-            -s "$sample" \
-            -i 'GT!="0/0" &CSQ_Consequence="intron_variant"' \
-            -f '[%SAMPLE]\t%CHROM\t%POS\t%REF\t%ALT\t[%GT]\t%FILTER\t%CSQ_Consequence\t%CSQ_gnomADg_AF\t%CSQ_gnomADe_AF\t%CSQ_TWE_WES_v1_AF\n' \
-            lepr_variants_TWE_37_sorted_annotated_filtered.vcf.gz | uniq )
-        count_intron=$(echo "$intron_variants" | wc -l)
-        if [ $count_intron -ge 1 ]; then
-            echo "$hets" >> single_het_intron_37.txt
-            echo "$intron_variants" >> single_het_intron_37.txt
+            lepr_variants_TWE_37_sorted_annotated_filtered.vcf.gz| uniq)
+            if [ "$intron_variants_old_annotation" = "" ]; then
+                intron_variants_new_annotation=$(bcftools query \
+                -s "$sample" \
+                -i 'GT!="0/0" &CSQ_Consequence="intron_variant"' \
+                -f '[%SAMPLE]\t%CHROM\t%POS\t%REF\t%ALT\t[%GT]\t%FILTER\t%CSQ_Consequence\t%CSQ_gnomADg_AF\t%CSQ_gnomADe_AF\t%CSQ_TWE_WES_v1_AF\n' \
+                lepr_variants_TWE_37_sorted_annotated_filtered.vcf.gz | uniq )
+
+                count_intron=$(echo "$intron_variants_new_annotation" | wc -l)
+                if [ $count_intron -ge 1 ]; then
+                    echo "$hets" >> single_het_intron_37.txt
+                    echo "$intron_variants_new_annotation" >> single_het_intron_37.txt
+                fi
+            else
+
+                count_intron=$(echo "$intron_variants_old_annotation" | wc -l)
+                if [ $count_intron -ge 1 ]; then
+                    echo "$hets" >> single_het_intron_37.txt
+                    echo "$intron_variants_old_annotation" >> single_het_intron_37.txt
+                fi
         fi
 
-    # print intronic variants
 
     fi
 done
